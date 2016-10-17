d9454fb9e28aa8e5758b03fe13cea1571845b683
i2bskn <i2bskn@gmail.com>
2016-10-17 23:47:41 +0900

    compatible test

--- compatible.go
+++ compatible.go
@@ -5,10 +5,12 @@ import (
 	"net/http"
 )
 
+// CompatibleHandler is pygmy.Handler wrapper for the compatible to http.Handler
 type CompatibleHandler struct {
 	Handler
 }
 
+// Compatible is returns http.Handler by pygmy.Handler
 func Compatible(handler Handler) http.Handler {
 	return CompatibleHandler{
 		Handler: handler,
@@ -19,6 +21,7 @@ func (c CompatibleHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	c.Handler.ServeHTTP(nil, w, r)
 }
 
+// HTTPHandlerWrapper is http.Handler wrapper for the compatible to pygmy.Handler
 type HTTPHandlerWrapper struct {
 	http.Handler
 }
---
+++ compatible_test.go
@@ -0,0 +1,42 @@
+package pygmy
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func fakeCompatibleHandler(want string) http.Handler {
+	mux := New()
+	mux.HandleFunc("/", func(ctx context.Context, w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, want)
+	})
+	return Compatible(mux)
+}
+
+func TestCompatible(_ *testing.T) {
+	var _ http.Handler = Compatible(New())
+}
+
+func TestCompatibleHandler(t *testing.T) {
+	want := "Hello Pygmy!"
+	ts := httptest.NewServer(fakeCompatibleHandler(want))
+	defer ts.Close()
+
+	r, err := http.Get(ts.URL)
+	if err != nil {
+		t.Fatalf("http.Get: %v", err)
+	}
+
+	data, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		t.Fatalf("ioutil.ReadAll: %v", err)
+	}
+
+	if want != string(data) {
+		t.Fatalf("got: %#v\nwant: %#v", string(data), want)
+	}
+}
